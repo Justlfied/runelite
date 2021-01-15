5655c34feb9acc87b18fa7c8228c6acddf1f085c
Mike Justlfed <Miketrainee9@live.nl>
2021-01-15 19:20:56 +0100

    Added Item Price and Item High Alch Price to the Discord Webhook

--- runelite-client/src/main/java/net/runelite/client/plugins/discordraredropnotifier/DiscordRareDropNotifierPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/discordraredropnotifier/DiscordRareDropNotifierPlugin.java
@@ -2,22 +2,16 @@ package net.runelite.client.plugins.discordraredropnotifier;
 
 import lombok.SneakyThrows;
 import net.runelite.api.Client;
-import net.runelite.client.RuneLite;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.NpcLootReceived;
+import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-
 import javax.inject.Inject;
-import javax.swing.*;
-import javax.swing.text.html.HTMLEditorKit;
-
 import okhttp3.*;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.awt.*;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.Arrays;
@@ -34,11 +28,15 @@ public class DiscordRareDropNotifierPlugin extends Plugin {
     private static final String API_PATH_ITEMS = "items";
 
     private OkHttpClient httpClient = null;
-    public static final MediaType JSON
-        = MediaType.parse("application/json; charset=utf-8");
 
     @Inject
     private Client client;
+
+    @Inject
+    private ItemManager itemManager;
+
+    @Inject
+    private ClientThread clientThread;
 
     @Subscribe
     public void onNpcLootReceived(NpcLootReceived npcLootReceived) {
@@ -70,7 +68,7 @@ public class DiscordRareDropNotifierPlugin extends Plugin {
 
                     @SneakyThrows
                     @Override
-                    public void onResponse(Call call, Response response) throws IOException {
+                    public void onResponse(Call call, Response response) {
                         if(response.isSuccessful()) {
                             String responseBody = response.body().string();
 
@@ -82,22 +80,32 @@ public class DiscordRareDropNotifierPlugin extends Plugin {
 
                                 if(object.getInt("id") == itemID) {
                                     DecimalFormat df = new DecimalFormat("#.000");
-                                    String webhookRarityField = " 1/" + 1/object.getDouble("rarity") + " (" + df.format(object.getDouble("rarity"))  + "%)";
+                                    String webhookRarityField = " 1/" + 1/object.getDouble("rarity") + " (" + df.format(100 * object.getDouble("rarity"))  + "%)";
                                     String webhookDescription = "Just got " + itemStack.getQuantity() + "x "
                                         + object.getString("name")
                                         + " from lvl " + jsonObject.getInt("combat_level")
                                         + " " + jsonObject.getString("name");
 
-                                    DiscordWebhook webhook = new DiscordWebhook("https://discord.com/api/webhooks/791336920365006860/dXBe90zwHfMWDB9idrZywz7tuSSkpSeWDket0mQAFKZatQM13aNagfnRqmUCaVKuU1oN");
-                                    webhook.setUsername("Gains");
-                                    webhook.setTts(false);
-                                    webhook.addEmbed(new DiscordWebhook.EmbedObject()
-                                        .setDescription(webhookDescription)
-                                        .addField("Rarity", "```#" + webhookRarityField + "```", true)
-                                        .addField("HA Value", "idfk", true)
-                                        .addField("GE Value", "idfk either", true)
-                                        .setAuthor(client.getLocalPlayer().getName(), "", ""));
-                                    webhook.execute();
+                                    clientThread.invokeLater(() -> {
+                                        try {
+                                            final int gePrice = itemManager.getItemPrice(itemID);
+                                            final int haPrice = itemManager.getItemComposition(itemID).getHaPrice();
+
+                                            DiscordWebhook webhook = new DiscordWebhook("https://discord.com/api/webhooks/791336920365006860/dXBe90zwHfMWDB9idrZywz7tuSSkpSeWDket0mQAFKZatQM13aNagfnRqmUCaVKuU1oN");
+                                            webhook.setUsername("Justy");
+                                            webhook.setTts(false);
+                                            webhook.addEmbed(new DiscordWebhook.EmbedObject()
+                                                .setDescription(webhookDescription)
+                                                .addField("Rarity", "```#" + webhookRarityField + "```", true)
+                                                .addField("HA Value", "```" + haPrice + "```", true)
+                                                .addField("GE Value", "```" + gePrice + "```", true)
+                                                .setAuthor(client.getLocalPlayer().getName(), "", ""));
+                                            webhook.execute();
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    });
+
                                 }
                             }
                             response.close();
