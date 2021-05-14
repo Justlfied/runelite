2fac1ce72b51481b1ad6003142f88d518dad613d
Justy <Miketrainee9@live.nl>
2021-05-14 18:51:13 +0200

    Twitch start

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/baldylitetwitch/TwitchApi.java
@@ -0,0 +1,4 @@
+package net.runelite.client.plugins.baldylitetwitch;
+
+public class TwitchApi {
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/baldylitetwitch/TwitchAuth.java
@@ -0,0 +1,8 @@
+package net.runelite.client.plugins.baldylitetwitch;
+
+// Client ID: vndi6h6etwnnvbulw0i2i9vl7d6kz0
+// Secret: h0bb664orgsq5jb29hwme480x1h879
+
+public class TwitchAuth {
+
+}
