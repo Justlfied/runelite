888f3545cd52cdbc589c9a37e440f26065fa1e79
Justlfied <57233854+Justlfied@users.noreply.github.com>
2021-06-16 08:36:34 +0200

    Start Decider

--- runelite-client/src/main/java/net/runelite/client/plugins/tobhelper/ToBModeDecider.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/tobhelper/ToBModeDecider.java
@@ -6,13 +6,22 @@ import net.runelite.client.eventbus.Subscribe;
 
 public class ToBModeDecider {
     private TobHelperNpcEnum tobHelperNpcEnum;
+    private TobHelperModeEnum tobHelperModeEnum;
+    
+    @Inject
+    private TobHelperPlugin plugin;
+    
+    private ToBModeDecier(TobHelperPlugin plugin) {
+        this.plugin = plugin;   
+    }
 
     @Subscribe
     public void onNpcSpawned(NpcSpawned npc) {
         int npcId = npc.getNpc().getId();
-        String test = tobHelperNpcEnum::getEnumKeyFromId();
-        if(tobHelperNpcEnum::getEnumKeyFromId(npcId)) {
-
+        String npcKey = TobHelperNpcEnum.getEnumKeyFromId(npcId);
+        if(npcKey && npcKey != "end") {
+            this.plugin.mode = npcKey;
+            this.plugin.setMode()
         }
     }
 }
